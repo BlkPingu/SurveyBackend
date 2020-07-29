4e86df58a037c26de07f663277eeee9af6f83c7e
BlkPingu <tobias-kolb@hotmail.de>
2020-07-29 12:09:24 +0200

    edit req txt

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 
 if app.config["ENV"] == "production":
-        app.config.from_object("config.ProductionConfig")
-    else:
-        app.config.from_object("config.DevelopmentConfig")
+    app.config.from_object("config.ProductionConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
 
     print(f'ENV is set to: {app.config["ENV"]}')
 
