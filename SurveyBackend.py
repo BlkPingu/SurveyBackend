06d256597dec801e2b1250de45a806c7d6f3ee12
BlkPingu <tobias-kolb@hotmail.de>
2020-08-04 22:52:47 +0200

    allow origin issue fix attempt

--- SurveyBackend.py
+++ SurveyBackend.py
@@ -24,6 +24,12 @@ else:
 
 CORS(app, resources={ r'/*': {'origins': '*'}}, supports_credentials=True)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 
 db = SQLAlchemy(app)
